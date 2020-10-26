b504dfdca93663efee8a7c215c993cd5c3efea28
ghrl1121 <70801233+ghrl1121@users.noreply.github.com>
2020-10-27 07:22:00 +0900

    Update map.bat

--- map.bat
+++ map.bat
@@ -1,5 +1,7 @@
 @echo off
 cls
 echo 새티스팩토리 세이브파일 찾기용입니다
+echo 세이브 파일 가지고 맵을 볼 수 있도록 링크 걸어 났습니다...(나중에 수정)
 explorer "%AppData%\..\Local\FactoryGame\Saved\SaveGames\"
+explorer "https://satisfactory-calculator.com/en/interactive-map#2;50302;0|gameLayer|limestonePure;ironPure;copperPure;cateriumPure;coalPure;oilPure;hardDrives"
 pause
